b6a72a2818500f3c074f1c54e1e089675f1bc804
madhums0411 <madhums41175@gmail.com>
2022-08-03 20:54:47 +0530

    Create Binary node

--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Welcome to Binary Search Tree Problem");
+﻿using BinarySearchTree;
+
+
+Console.WriteLine("Welcome To Binary Search Tree Problem");
+BinarySearchTree<int> BinaryTree = new BinarySearchTree<int>();
+
+BinaryTree.AddNode(56);
+BinaryTree.AddNode(30);
+BinaryTree.AddNode(70);
+BinaryTree.AddNode(22);
+BinaryTree.AddNode(40);
+BinaryTree.AddNode(60);
+BinaryTree.AddNode(95);
+BinaryTree.AddNode(11);
+BinaryTree.AddNode(65);
+BinaryTree.AddNode(33);
+BinaryTree.AddNode(16);
+BinaryTree.AddNode(63);
+BinaryTree.AddNode(67);
